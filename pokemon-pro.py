8975db23b54a79392ca63fff97bbe66dd3b2bfc9
Sekhmet369 <102258702+Sekhmet369@users.noreply.github.com>
2022-05-20 15:59:34 +0100

    main code

---
+++ pokemon-pro.py
@@ -0,0 +1,21 @@
+
+# greet player, get name and if want to generate or choose pokemons
+
+# generate list of 10 rand numbers (1-151) without duplicities
+
+# if choosing pokemons, by names of pokemon find IDs from all_pokemons and overwrite first 5 entries in the list
+
+# KATARINA - pull pokemons to 2 arrays of 5
+
+# choose pokemon for combat round and safeguard on 0 HP for human
+
+# choose pokemon for combat round and safeguard on 0 HP for computer
+
+# KATARINA - combat round = New HP = HP - (attack-defense)
+
+# game run - repetition of combat until all cards 0 hp
+
+# announce result
+
+# if win read highscores.csv to array, append new win and desc sort by win likelyhood (sum of hp+attack+defense/computer in %)
+# highscores.csv - timestamp, player name, pokemons in team, opponent team, win likelyhood
