e408fcb79af946ac76dc08fb2d7807a42c49d905
Sekhmet369 <102258702+Sekhmet369@users.noreply.github.com>
2022-05-23 21:51:26 +0100

    Update pokemon-pro.py

--- pokemon-pro.py
+++ pokemon-pro.py
@@ -1,15 +1,26 @@
+#IMPORT ALL MODULES REQUIRED
+import requests
+import random
+import pandas as pd
 
 # KATIE - greet player, get name and if want to generate or choose pokemons
 
-# generate list of 10 rand numbers (1-151) without duplicities
+# KATARINA  - generate list of 10 rand numbers (1-151) without duplicities
+contains_duplicates=True
+while contains_duplicates is not False:
+    pokemon_list=random.sample(range(0,100), 10)
+    print(pokemon_list)
+    id_set = set(pokemon_list)
+    contains_duplicates = len(pokemon_list) != len(id_set)
+    print(contains_duplicates)
 
 # if choosing pokemons, by names of pokemon find IDs from all_pokemons and overwrite first 5 entries in the list
 
 # KATARINA - pull pokemons to 2 arrays of 5
 
-# choose pokemon for combat round and safeguard on 0 HP for human
+# KATARINA - choose pokemon for combat round and safeguard on 0 HP for human
 
-# choose pokemon for combat round and safeguard on 0 HP for computer
+# KATARINA - choose pokemon for combat round and safeguard on 0 HP for computer
 
 # KATARINA - combat round = New HP = HP - (attack-defense)
 
